1c2f1da2c31a8be70760272e26541f3b5dd47cad
Fajran Iman Rusadi <fajran@gmail.com>
2017-05-02 05:34:31 +0200

    Fix bug

--- pkg/buildlog/buildlog.go
+++ pkg/buildlog/buildlog.go
@@ -28,7 +28,7 @@ func NewBuildLog(db *sql.DB) *BuildLog {
 
 func (bl *BuildLog) Create(key string) (*Build, error) {
 	var id int
-	err := bl.db.QueryRow(`INSERT INTO buildlog (key) VALUES ($1) RETURNING id`, key).Scan(id)
+	err := bl.db.QueryRow(`INSERT INTO buildlog (key) VALUES ($1) RETURNING id`, key).Scan(&id)
 	if err != nil {
 		return nil, err
 	}
